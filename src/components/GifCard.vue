82b29e34036d65b8b48f5e64a461da552be7f702
Cagatay Kula <cagatay.kula@barn.com.tr>
2019-09-30 08:45:47 +0300

    gif component added

---
+++ src/components/GifCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="gifCardWrapper">
+    <img
+      :src="src"
+      class="gif"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.gifCardWrapper {
+  transition: all .3s ease;
+  margin: 10px;
+}
+.gifCardWrapper:hover {
+  transform: scale(1.1, 1.1);
+}
+.gif {
+  width: 200px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="gifs">
+    <GifCard
+      v-for="item in gifs"
+      :key="item.id"
+      :src="item.images.fixed_height.url"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
+import GifCard from '@/components/GifCard.vue';
 
 export default {
   name: 'home',
+  data(){
+    return{
+      gifs: {}
+    }
+  },
+  created: function () {
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=nbAmrxeq4s6nOvEA4CUtgiFHNGa7JCMn&limit=20')
+      .then(response => response.data)
+      .then((response) => {
+        console.log(response);
+        this.gifs = response.data;
+      });
+  },
   components: {
-    HelloWorld,
+    GifCard,
   },
 };
 </script>
+
+<style scoped>
+.gifs {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  flex-wrap: wrap;
+}
+</style>
